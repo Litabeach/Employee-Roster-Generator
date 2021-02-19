c625433792b8f132415ddcc3e2410e280bec9cf4
Lita Beach <litabeach123@gmail.com>
2021-02-19 11:29:30 -0600

    all classes completed

--- 02-Homework/Develop/lib/Intern.js
+++ 02-Homework/Develop/lib/Intern.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+class Intern extends Employee {
+        constructor(name, id, email, school) {
+          super(name, id, email);
+          this.school = school
+        }
+      get school(){
+      return this.school
+        }
+      get role(){
+          return Intern
+      }  
+        
+      }
+      
+      module.exports = Intern;
--- 02-Homework/Develop/lib/Manager.js
+++ 02-Homework/Develop/lib/Manager.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+class Manager extends Employee {
+        constructor(name, id, email, officeNumber) {
+          super(name, id, email);
+          this.officeNumber = officeNumber
+        }
+      get officeNumber(){
+      return this.officeNumber
+        }
+      get role(){
+          return Manager
+      }  
+        
+      }
+      
+      module.exports = Manager;
