ee902989f1a47ebe259002e87e0da92ca5e81434
Lita Beach <litabeach123@gmail.com>
2021-02-20 16:24:24 -0600

    added a style sheet

--- 02-Homework/Develop/lib/Intern.js
+++ 02-Homework/Develop/lib/Intern.js
@@ -1,8 +1,4 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 const Employee = require("./Employee")
-
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
 class Intern extends Employee {
         constructor(name, id, email, school) {
--- 02-Homework/Develop/lib/Manager.js
+++ 02-Homework/Develop/lib/Manager.js
@@ -1,7 +1,4 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 const Employee = require("./Employee")
-
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
 class Manager extends Employee {
         constructor(name, id, email, officeNumber) {
